73bf6c1777394a08275e6def7db6fb54f3102844
ausnatki <109503108+ausnatki@users.noreply.github.com>
2023-12-02 01:36:17 +0800

    Add files via upload

---
+++ MVC做不来的作业/MVC做不来的作业/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC做不来的作业.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
