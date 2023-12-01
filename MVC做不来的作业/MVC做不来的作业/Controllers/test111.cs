2b163dce3d4b4af3bfe1725b057909fb56ee3ab8
ausnatki <109503108+ausnatki@users.noreply.github.com>
2023-12-02 01:40:16 +0800

    111

---
+++ MVC做不来的作业/MVC做不来的作业/Controllers/test111.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC做不来的作业.Controllers
+{
+    public class test111 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
