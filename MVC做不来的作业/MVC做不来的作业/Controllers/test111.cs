cb54680f0ca9f00becab2da129fb197d1179dbe8
blk420 <2387105276@qq.com>
2023-12-02 14:40:16 +0800

    111

--- MVC做不来的作业/MVC做不来的作业/Controllers/test111.cs
+++ MVC做不来的作业/MVC做不来的作业/Controllers/test111.cs
@@ -16,7 +16,7 @@ namespace MVC做不来的作业.Controllers
         public ActionResult Test111() 
         {
 
-            string ang = "这是我分支2的修改";
+            string ang = "这是我分支1的修改";
 
             int id = 11;
 
